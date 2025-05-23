60de95d80b03f515ae68cd648f27bcb60363229b
Hansimov <591172499@qq.com>
2025-05-23 16:04:11 +0800

    :zap: [Enhance] add include_alpha_num param in calc_cjk_char_len

--- src/btok/utils.py
+++ src/btok/utils.py
@@ -1,4 +1,5 @@
-from .constants import CH_MASK, PT_ATOZ_WS, PT_CH_CJK
+from .constants import CH_MASK, PT_ATOZ_WS
+from .categorizer import PT_CATOG
 
 
 def mask_ws_between_atoz(sentence: str) -> str:
@@ -9,5 +10,16 @@ def replace_mask_to_ws(tokens: list[str]):
     return [token.replace(CH_MASK, " ") for token in tokens]
 
 
-def calc_cjk_char_len(token: str) -> int:
-    return sum(1 for char in token if PT_CH_CJK.match(char))
+def calc_cjk_char_len(sent: str, include_alphanum: bool = True) -> int:
+    cjk_char_len = 0
+    for match in PT_CATOG.finditer(sent):
+        token_text = match.group(0)
+        token_type = match.lastgroup
+        if token_type == "cjk":
+            cjk_char_len += len(token_text)
+        elif token_type in ["arab", "atoz"]:
+            if include_alphanum:
+                cjk_char_len += len(token_text.split())
+        else:
+            continue
+    return cjk_char_len
